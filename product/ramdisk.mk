85d9866ce109616b49a3ed9ae073753720216439
Mohd Faraz <Checkingprop@gmail.com>
2017-04-12 22:58:43 +0530

    Update ramdisk.mk

--- product/ramdisk.mk
+++ product/ramdisk.mk
@@ -1,3 +1,4 @@
+# Ramdisk
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/enableswap.sh:root/enableswap.sh \
     $(LOCAL_PATH)/rootdir/factory_init.project.rc:root/factory_init.project.rc \
@@ -5,7 +6,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/fstab.charger:root/fstab.charger \
     $(LOCAL_PATH)/rootdir/fstab.mt6735:root/fstab.mt6735 \
     $(LOCAL_PATH)/rootdir/init.modem.rc:root/init.modem.rc \
-	$(LOCAL_PATH)/rootdir/init.microtrust.rc:root/init.microtrust.rc \
     $(LOCAL_PATH)/rootdir/init.mt6735.rc:root/init.mt6735.rc \
     $(LOCAL_PATH)/rootdir/init.mt6735.usb.rc:root/init.mt6735.usb.rc \
     $(LOCAL_PATH)/rootdir/init.project.rc:root/init.project.rc \
