cddb40efa4e9dc39edd216effa207d3518320afd
FARAZ <mohd.faraz.abc@gmail.com>
2017-04-12 14:12:40 +0530

    Initial Commit

---
+++ libmtk_symbols/mtk_cam.cpp
@@ -0,0 +1,3 @@
+extern "C" {
+    void _ZNK7android14Cameraservice22getInterfaceDescriptorEv();
+}
---
+++ product/camera.mk
@@ -0,0 +1,9 @@
+# Camera
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
+    $(LOCAL_PATH)/configs/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml
+
+PRODUCT_PACKAGES += \
+    Snap
