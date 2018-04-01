9604634936b1ed6b8c9a9317343b80bac52d1451
Mohd Faraz <mohd.faraz.abc@gmail.com>
2018-04-01 06:35:25 +0000

    G42: Added Support for PitchBlack Recovery

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -57,3 +57,5 @@ TARGET_SYSTEM_PROP := $(LOCAL_PATH)/system.prop
 # Block based ota
 # see http://review.cyanogenmod.org/#/c/78849/1/core/Makefile
 BLOCK_BASED_OTA := false
+
+DEVICE_HAS_2GB_VARIANT := true
